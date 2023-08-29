38ec8555a315d7f4e30e98c4d23829a14d1fa1c1
UkiDelly <daehyeon1999@naver.com>
2023-08-29 15:32:45 +0900

    [UPDATE] index.ts
    -GPT에게 물어보고 대답 얻는 과정 테스트 (성공)

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
-
-
-
-
-
-
-
-
-
+import { askGPT } from "./gpt/gpt.js";
+import { Model, SystemModel, UserModel } from "./model/role_model.js";
+const systemRole = new SystemModel('assistant는 전문성 있는 글을 쓰는 전문가야')
+const data: Model[] = [systemRole];
 
 
 
 
 // =========== 실행 ============
+
+async function main() {
+  console.log(data);
+  const question = new UserModel("이유를 모르겠지만 실험은 실패했다.")
+  data.push(question)
+
+
+  const result = await askGPT(data)
+  console.log(result);
+
+}
+
+main()
+
